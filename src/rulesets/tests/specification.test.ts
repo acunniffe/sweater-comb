f1d18b492388109c02d41abfcb502624c88cfbda
Aidan Cunniffe <acunniffe@gmail.com>
2022-03-18 20:58:58 -0400

    repro

--- src/rulesets/tests/specification.test.ts
+++ src/rulesets/tests/specification.test.ts
@@ -2,6 +2,7 @@ import { rules } from "../specification";
 import { SynkApiCheckContext } from "../../dsl";
 
 import { createSnykTestFixture } from "./fixtures";
+import { OpenAPIV3 } from "@useoptic/openapi-utilities";
 const { compare } = createSnykTestFixture();
 
 const emptyContext: SynkApiCheckContext = {
@@ -13,3 +14,50 @@ const emptyContext: SynkApiCheckContext = {
   },
   resourceVersions: {},
 };
+
+const baseOpenAPI: OpenAPIV3.Document = {
+  openapi: "3.0.1",
+  paths: {
+    "/example": {
+      get: {
+        responses: {
+          "200": {
+            description: "",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    // links: { $ref: "#/components/x-snyk-common/schemas/Links" },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  info: { version: "0.0.0", title: "OpenAPI" },
+
+  components: {
+    // @ts-ignore
+    "x-snyk-common": {
+      $ref: "https://raw.githubusercontent.com/snyk/sweater-comb/common-model-v1/components/common.yaml",
+    },
+    schemas: {
+      HelloWorld: {
+        type: "object",
+      },
+    },
+  },
+};
+
+it("allows non-snake case if already in spec", async () => {
+  const result = await compare(baseOpenAPI)
+    .to((s) => s)
+    .withRule(rules.componentNameCase, emptyContext);
+
+  expect(result.results);
+  expect(result).toMatchSnapshot();
+});
